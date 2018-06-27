a04cfeeb5dc880dd8b42544d298c5d13da57e19f
Matthew Smailey <msmailey@networthservices.com>
2018-06-27 11:47:51 -0700

    Initial commit

---
+++ fish/config.fish
@@ -0,0 +1,4 @@
+alias ls='ls --color=auto'
+alias werk='cd /home/matthew/Documents/Work/Merged'
+alias grep='grep -s --exclude-dir=node_modules --exclude-dir=.git --exclude=bundle.js --exclude=package-lock.json --color=always'
+neofetch
---
+++ fish/functions/fzf_key_bindings.fish
@@ -0,0 +1,1 @@
+/home/matthew/.fzf/shell/key-bindings.fish
