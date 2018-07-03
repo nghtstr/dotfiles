cec6685ba750919146262c54f945929c681bedb3
Matthew Smailey <msmailey@networthservices.com>
2018-07-03 16:45:34 -0700

    Added vscode vim keybindings

---
+++ vscode/keybindings.js
@@ -0,0 +1,8 @@
+// Place your key bindings in this file to overwrite the defaults
+[
+    { "key": "ctrl+f", "command": "actions.find" },
+    { "key": "ctrl+h", "command": "editor.action.startFindReplaceAction" },
+    { "key": "ctrl+a", "command": "editor.action.selectAll" },
+    { "key": "ctrl+h", "command": "workbench.action.focusSideBar" },
+    { "key": "ctrl+l", "command": "workbench.action.focusNextGroup" },
+]
