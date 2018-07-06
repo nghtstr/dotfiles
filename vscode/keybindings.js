8df8c30658322d90866f41f76eacc2a113076271
Matthew Smailey <msmailey@networthservices.com>
2018-07-06 12:08:22 -0700

    Added missing vscode keybindings

--- vscode/keybindings.js
+++ vscode/keybindings.js
@@ -3,6 +3,32 @@
     { "key": "ctrl+f", "command": "actions.find" },
     { "key": "ctrl+h", "command": "editor.action.startFindReplaceAction" },
     { "key": "ctrl+a", "command": "editor.action.selectAll" },
-    { "key": "ctrl+h", "command": "workbench.action.focusSideBar" },
-    { "key": "ctrl+l", "command": "workbench.action.focusNextGroup" },
+    {
+        "key": "ctrl+h",
+        "command": "workbench.action.focusSideBar"
+    },
+    {
+        "key": "ctrl+l",
+        "command": "workbench.action.focusFirstEditorGroup"
+    },
+    {
+        "key": "ctrl+1",
+        "command": "-workbench.action.focusFirstEditorGroup"
+    },
+    {
+        "key": "ctrl+k",
+        "command": "workbench.action.nextEditor"
+    },
+    {
+        "key": "ctrl+pagedown",
+        "command": "-workbench.action.nextEditor"
+    },
+    {
+        "key": "ctrl+j",
+        "command": "workbench.action.previousEditor"
+    },
+    {
+        "key": "ctrl+pageup",
+        "command": "-workbench.action.previousEditor"
+    }
 ]
