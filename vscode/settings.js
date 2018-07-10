6649c8584d293e3f6564d2434fd1184b02cebeba
Matthew Smailey <msmailey@networthservices.com>
2018-07-10 15:02:58 -0700

    Added vscode settings

---
+++ vscode/settings.js
@@ -0,0 +1,7 @@
+{
+  "workbench.colorTheme": "Flatland Monokai",
+  "workbench.iconTheme": "material-icon-theme",
+  "editor.fontFamily": "'Noto Sans Mono, Medium'",
+  "editor.fontSize": 16,
+  "window.menuBarVisibility": "toggle",
+}
