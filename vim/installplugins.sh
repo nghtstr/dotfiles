b9510130371caa3ded3c4bfe639d37591ea652fe
Matthew Smailey <msmailey@networthservices.com>
2018-07-12 17:34:10 -0700

    Fixed vim install script

--- vim/installplugins.sh
+++ vim/installplugins.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-git clone https://github.com/lightline-ale ~/.vim/bundle/lightline-ale
-git clone https://github.com/lightline.vim ~/.vim/bundle/lightline
-git clone https://github.com/nerdtree ~/.vim/bundle/nerdtree
-git clone https://github.com/Vundle.vim ~/.vim/bundle/
-git clone https://github.com/editorconfig-vim.git ~/.vim/bundle/
+git clone https://github.com/maximbaz/lightline-ale ~/.vim/bundle/lightline-ale
+git clone https://github.com/itchyny/lightline.vim ~/.vim/bundle/lightline
+git clone https://github.com/scrooloose/nerdtree ~/.vim/bundle/nerdtree
+git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/
+git clone https://github.com/editorconfig/editorconfig-vim.git ~/.vim/bundle/
 git clone https://github.com/mxw/vim-jsx.git ~/.vim/bundle/vim-jsx
 
