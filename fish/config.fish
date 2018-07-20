0c94f4c75a363928e1e549474ff1f632cf2b7e3e
Matthew Smailey <msmailey@networthservices.com>
2018-07-20 10:22:47 -0700

    Updated fish alias

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
 alias ls='ls --color=auto'
-alias werk='cd /home/matthew/Documents/Work/Merged'
+alias werk='cd /home/matthew/Development/networth/'
 alias grep='grep -s --exclude-dir=node_modules --exclude-dir=.git --exclude=bundle.js --exclude=package-lock.json --color=always'
 neofetch
