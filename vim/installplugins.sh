c7b65a613baf22325fdc39095b4d7908f784187d
Matthew Smailey <msmailey@networthservices.com>
2018-07-24 10:56:40 -0700

    Fixed vim installplugin script

--- vim/installplugins.sh
+++ vim/installplugins.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 mkdir -p ~/.vim/autoload ~/.vim/bundle
+mkdir -p ~/.vim/colors
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/maximbaz/lightline-ale ~/.vim/bundle/lightline-ale
 git clone https://github.com/itchyny/lightline.vim ~/.vim/bundle/lightline
@@ -8,5 +9,5 @@ git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/
 git clone https://github.com/editorconfig/editorconfig-vim.git ~/.vim/bundle/
 git clone https://github.com/mxw/vim-jsx.git ~/.vim/bundle/vim-jsx
 git clone https://github.com/ryanoasis/vim-devicons ~/.vim/bundle/vim-devicons
-wget https://raw.githubusercontent.com/Reewr/vim-monokai-phoenix/master/colors/monokai-phoenix.vim ~/.vim/colors/monokai-phoenix.vim 
+wget https://raw.githubusercontent.com/Reewr/vim-monokai-phoenix/master/colors/monokai-phoenix.vim -O ~/.vim/colors/monokai-phoenix.vim 
 pacaur -S aur/nerd-fonts-complete
