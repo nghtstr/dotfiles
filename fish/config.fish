4b346dd77146620492c99cb6079c3484db4b86d8
Matthew Smailey <msmailey@networthservices.com>
2018-09-26 12:10:10 -0700

    Small tweaks

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
-alias ls='ls --color=auto'
+alias ls='ls --color'
 alias werk='cd /home/matthew/Development/networth/'
 alias grep='grep -s --exclude-dir=node_modules --exclude-dir=.git --exclude=bundle.js --exclude=package-lock.json --color=always'
 neofetch
