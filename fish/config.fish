c2638b221a34ecf8b6029a92aa0b3805b4480fa7
Matthew Smailey <msmailey@networthservices.com>
2019-03-28 10:38:07 -0700

    Replace home path with ~ in fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
 alias ls='ls --color'
 alias werk='cd /home/matthew/Development/networth/'
 alias nssh='ssh -i ~/.keys/aws.networth.pem'
-alias nscp='scp -i /home/matthew/.keys/aws.networth.pem'
+alias nscp='scp -i ~/.keys/aws.networth.pem'
 alias grep='grep -s --exclude-dir=node_modules --exclude-dir=.git --exclude=bundle.js --exclude=package-lock.json --color=always'
 neofetch
