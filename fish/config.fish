ba426e7e2e50f694211dbdab6d733290fa48a310
Matthew Smailey <msmailey@networthservices.com>
2019-04-10 11:55:41 -0700

    Minor changes

--- fish/config.fish
+++ fish/config.fish
@@ -3,4 +3,6 @@ alias werk='cd /home/matthew/Development/networth/'
 alias nssh='ssh -i ~/.keys/aws.networth.pem'
 alias nscp='scp -i ~/.keys/aws.networth.pem'
 alias grep='grep -s --exclude-dir=node_modules --exclude-dir=.git --exclude=bundle.js --exclude=package-lock.json --color=always'
+alias ll='ls -al'
+alias youtube='firefox --new-window https://www.youtube.com'
 neofetch
