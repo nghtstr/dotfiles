6367ea7e50f59c2c25fb5954e7579056f64d04bc
Matthew Smailey <msmailey@networthservices.com>
2019-04-18 15:08:25 -0700

    Added `cb` clipboard alias

--- fish/config.fish
+++ fish/config.fish
@@ -5,4 +5,5 @@ alias nscp='scp -i ~/.keys/aws.networth.pem'
 alias grep='grep -s --exclude-dir=node_modules --exclude-dir=.git --exclude=bundle.js --exclude=package-lock.json --color=always'
 alias ll='ls -al'
 alias youtube='firefox --new-window https://www.youtube.com'
+alias cb='xclip -sel clip'
 neofetch
