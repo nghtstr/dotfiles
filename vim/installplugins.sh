ae63716078d19910df2d61dded364df3c935432d
Matthew Smailey <them4tthew@gmail.com>
2022-03-31 11:48:02 -0700

    added ale plugin

--- vim/installplugins.sh
+++ vim/installplugins.sh
@@ -11,6 +11,7 @@ git clone https://github.com/mxw/vim-jsx.git ~/.vim/bundle/vim-jsx
 git clone https://github.com/ryanoasis/vim-devicons ~/.vim/bundle/vim-devicons
 git clone https://github.com/airblade/vim-gitgutter ~/.vim/bundle/vim-gitgutter
 git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
+git clone https://github.com/dense-analysis/ale.git ~/.vim/bundle/ale
 
 wget https://raw.githubusercontent.com/Reewr/vim-monokai-phoenix/master/colors/monokai-phoenix.vim -O ~/.vim/colors/monokai-phoenix.vim 
 yay -S aur/nerd-fonts-complete
